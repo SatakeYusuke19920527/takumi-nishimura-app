3f9cc00c3295a942d04f36058bacb27416744816
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-02-13 20:29:13 +0900

    tableできた

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, Button,} from 'react'
 import { Link } from 'react-router-dom'
-import {Table} from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ComponentA = () => {
     const [ count, setCount ] = useState(0);
@@ -10,7 +11,7 @@ const ComponentA = () => {
     useEffect(() => {
         console.log('useEffect が呼び出されました。');
 
-        axios.get("https://jsonplaceholder.typicode.com/todos")
+        axios.get("https://jsonplaceholder.typicode.com/posts")
         .then(res => {
             console.log(res, 'res check')
             setData(res.data)
@@ -36,46 +37,38 @@ const ComponentA = () => {
             <div><button onClick={up}>up</button></div>
             <div><button onClick={down}>down</button></div>
             <div><button onClick={reset}>reset</button></div>
-            <div>数値 : {data.map(d => {
-                return(
-                    <div>
-                        {d.title}
-                    </div>
-                )
-            })}</div>
-            <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</Table>
+            <div><button onClick={start}>go</button></div>
+            
+            <Table striped bordered hover  >
+            <thead>
+              <tr>
+                <th>userID</th>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+                {data.map(u => {
+                          return(
+                                  <tr>
+                                    <td>{u.userId}</td>
+                                    <td>{u.id}</td>
+                                    <td>{u.title}</td>
+                                    <td>{u.body}</td>
+                                  </tr> 
+                          )
+                })}
+            </tbody>
+          </Table>
         </div>
-        
-
     )
+}
+
+const start = () => {
+  
+    
+  
 }
 
 export default ComponentA
